f109f41f50f5ec96aca91d81b614ce8bee16e5a8
randallHu <865584045@qq.com>
2019-03-31 12:30:45 +0800

    2.debug

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Left>
+
+    </Left>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Left from './components/left.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Left
   }
 }
 </script>
